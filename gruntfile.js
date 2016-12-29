4a48ae4ee69224a7ee8f557e421428cc56edaa21
Brent Rahn <brent.rahn@gmail.com>
2016-12-29 10:11:03 -0800

    Update dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -80,13 +80,13 @@ grunt.initConfig({
 	/*
 	compile html
 	*/
-	jade: {
+	pug: {
 		options: {
-			path: '<%= dirs.src %>/jade/'
+			path: '<%= dirs.src %>/pug/'
 		},
 		all: {
 			files: {
-				'<%= dirs.dest %>/index.html': '<%= dirs.src %>/jade/index.jade'
+				'<%= dirs.dest %>/index.html': '<%= dirs.src %>/pug/index.pug'
 			}
 		}
 	},
@@ -126,7 +126,7 @@ grunt.initConfig({
 
 // Load helpers
 grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-jade');
+grunt.loadNpmTasks('grunt-contrib-pug');
 grunt.loadNpmTasks('grunt-contrib-jshint');
 grunt.loadNpmTasks('grunt-contrib-stylus');
 grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -134,8 +134,8 @@ grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-github-pages');
 
 // Tasks (command line)
-grunt.registerTask('default', ['concat', 'jshint', 'jade', 'stylus']);
-grunt.registerTask('dist', ['jshint', 'uglify', 'jade', 'stylus']);
+grunt.registerTask('default', ['concat', 'jshint', 'pug', 'stylus']);
+grunt.registerTask('dist', ['jshint', 'uglify', 'pug', 'stylus']);
 // create an alias for the githubPages task
-grunt.registerTask('gh-pages', ['jshint', 'uglify', 'jade', 'stylus', 'githubPages:target']);
+grunt.registerTask('gh-pages', ['jshint', 'uglify', 'pug', 'stylus', 'githubPages:target']);
 };
